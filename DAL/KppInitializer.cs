07bfae9ceaccc886002aaa0934533fd3a2ee206a
Student <Student@vc.ksue.org.ua>
2017-04-10 13:02:02 +0300

    first commit

---
+++ DAL/KppInitializer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.Entities;
+
+namespace DAL
+{
+    public class KppInitializer : System.Data.Entity.DropCreateDatabaseIfModelChanges<KppContext>
+    {
+        protected override void Seed(KppContext context)
+        {
+            var visitors = new List<Visitor>
+            {
+              
+                new Visitor(){
+                    LastName = "Stepanov",
+                    FirstName = "Maxi",
+                    Age = 1,
+                    Email = "stepanovonline@gmail.com",
+                    Gender = "Male",
+                    Telephone = "+38068 25 06 773",
+                    Feedback = "All right!"}
+               
+            };
+
+            visitors.ForEach(s => context.Visitors.Add(s));
+            context.SaveChanges();
+        }
+    }
+}
